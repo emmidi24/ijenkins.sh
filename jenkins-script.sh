3ceff60c57bbb5966706de271ec172b21d264f2b
emmidi24 <emmidipraveen2468@gmail.com>
2024-11-05 12:24:45 +0530

    Update jenkins-script.sh

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt update 
 apt install openjdk-17-jre
+apt install maven
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
